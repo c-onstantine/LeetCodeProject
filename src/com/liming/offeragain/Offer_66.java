dd59396d5fd20134070bbc8432226d7360d08f4b
c-onstantine <1258379023@qq.com>
2021-11-29 09:23:16 +0800

    增加注释

--- src/com/liming/offeragain/Offer_66.java
+++ src/com/liming/offeragain/Offer_66.java
@@ -3,6 +3,7 @@ package com.liming.offeragain;
 /**
  * @author ltf
  * @date 2021-11-27 21:15
+ * 每个位置是其他位置元素的乘积   这个利用一维数组 构造一个矩阵  就能清晰的看出 如何利用累乘 求出对应位置的结果。
  */
 public class Offer_66 {
     public int[] constructArr(int[] a) {
