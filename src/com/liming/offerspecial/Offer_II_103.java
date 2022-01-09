47ac2d1723408c937042a5202d215c5cc7fe8d15
c-onstantine <1258379023@qq.com>
2022-01-09 11:32:27 +0800

    Offer 第35天 动态规划

---
+++ src/com/liming/offerspecial/Offer_II_103.java
@@ -0,0 +1,23 @@
+package com.liming.offerspecial;
+
+/**
+ * @author ltf
+ * @date 2022-01-09 10:46
+ */
+public class Offer_II_103 {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+        dp[0] = 0;
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                if (dp[j - coins[i]] != Integer.MAX_VALUE) {
+                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
---
+++ src/com/liming/offerspecial/Offer_II_104.java
@@ -0,0 +1,20 @@
+package com.liming.offerspecial;
+
+/**
+ * @author ltf
+ * @date 2022-01-09 11:32
+ */
+public class Offer_II_104 {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i >= nums[j]) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
